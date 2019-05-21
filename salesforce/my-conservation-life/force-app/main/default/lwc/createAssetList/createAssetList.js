fbd4ee05a63e1a97204a0a4da66a3c1c9c9aabaf
Brian Picciano <BJPicciano@gmail.com>
2019-05-21 18:19:12 -0400

    Add additional custom properties

--- salesforce/my-conservation-life/force-app/main/default/lwc/createAssetList/createAssetList.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/createAssetList/createAssetList.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class CreateAssetList extends LightningElement {}
+export default class CreateAssetList extends LightningElement {
+    id = 1;
+
+    @track
+    properties = [this.id];
+
+    addCustomProperty() {
+        this.id++;
+        this.properties.push(this.id);
+    }
+}
