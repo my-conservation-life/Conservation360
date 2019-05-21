7fa1d162f15c6ac0f2645e3715f4e3634c15c975
Brian Picciano <BJPicciano@gmail.com>
2019-05-21 19:01:09 -0400

    Created node api for querying postgres db Moved README.md and .gitignore to root folder

---
+++ api/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const {Client} = require("pg");
+const bodyParser = require("body-parser");
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const client = new Client({
+    connectionString: process.env.DB_URI,
+    ssl: true,
+});
+
+client.connect()
+    .then(() => {
+        console.log("Successfully connected to database");
+    })
+    .catch(e => {
+        console.error(e.stack);
+        client.end();
+    });
+
+app.get("/getDataTypes", async (req, res) => {
+    const query = `
+        SELECT name
+        FROM data_type
+    `;
+
+    try {
+        const db = await client.query(query);
+        const data = db.rows;
+
+        res.send(JSON.stringify(data));
+    } catch (e) {
+        const msg = {
+            message: "Unable to query database",
+            error: e.message
+        };
+        console.error(e.stack);
+        res.status(500).send(msg);
+    }
+});
+
+// Start server
+app.listen(port, () => console.log("Listening on port", port));
