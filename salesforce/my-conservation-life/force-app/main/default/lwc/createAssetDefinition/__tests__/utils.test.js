72a9d4b81b318bf7b0d7641bc15cd48ab5912bd7
Brian Picciano <BJPicciano@gmail.com>
2019-05-31 20:52:51 -0400

    Init jest tests createAssetDefinition returns assetType id

---
+++ salesforce/my-conservation-life/force-app/main/default/lwc/createAssetDefinition/__tests__/utils.test.js
@@ -0,0 +1,15 @@
+import { createElement } from 'lwc';
+import createAssetDefinition from 'c/createAssetDefinition';
+
+describe('c-create-asset-definition', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('new test', () => {
+        throw new Error('test needs implementing');
+    });
+});
---
+++ salesforce/my-conservation-life/force-app/main/default/lwc/utils/__tests__/utils.test.js
@@ -0,0 +1,15 @@
+import { createElement } from 'lwc';
+import utils from 'c/utils';
+
+describe('c-utils', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('new test', () => {
+        throw new Error('test needs implementing');
+    });
+});
