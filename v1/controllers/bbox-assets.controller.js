986cc8e021a3741ff3594bdc4861017207e4ebe2
James C. De Ricco <jcd7042@rit.edu>
2019-06-03 03:26:54 -0400

    bbox-assets endpoint: factor out DB access
    Created the bbox-assets.db.js module to contain DB
    access logic for the bbox-assets endpoint. This separates
    DB access from network request management.
    
    Also adds unit tests for bbox-assets.db.js

--- v1/controllers/bbox-assets.controller.js
+++ v1/controllers/bbox-assets.controller.js
@@ -1,31 +1,19 @@
-const { dbKeyParser, queryDbResult } = require('../db');
+const { dbKeyParser } = require('../db');
 const { withQueryParam } = require('../utils/request');
+const bboxAssetsDb = require('../db/bbox-assets.db');
 
-const get = async (req, res) => {
-    const createQuery = (fromTableExpression) => `
-        SELECT ST_XMin(bbox) AS latitude_min, ST_XMax(bbox) AS latitude_max, ST_YMin(bbox) AS longitude_min, ST_YMax(bbox) AS longitude_max
-        FROM (SELECT ST_Extent(location) AS bbox FROM ${fromTableExpression})
-        AS tbbox
-    `;
-
-    const withResult = (result) => {
-        const bbox = result.rows[0];
-        if (bbox) {
+const get = (req, res, next) => {
+    const onProjectIdParse = async (projectId) => {
+        try {
+            const bbox = await bboxAssetsDb.get(projectId);
             res.json(bbox);
-        } else {
-            res.status(500).send({ error: 'Unable to query DB for bounding box' });
+        } catch (error) {
+            next(error);
         }
     };
 
     withQueryParam(req, res, 'project_id', dbKeyParser,
-        (projectId) => {
-            const query = createQuery('asset WHERE project_id = $1')
-            queryDbResult(res, query, [projectId], withResult);
-        },
-        () => {
-            const query = createQuery('asset');
-            queryDbResult(res, query, [], withResult);
-        });
+        onProjectIdParse, onProjectIdParse);
 };
 
 module.exports = { get };
