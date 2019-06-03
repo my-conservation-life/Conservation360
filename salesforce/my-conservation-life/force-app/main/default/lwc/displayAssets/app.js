4a93c2b2dc2041e986d42f8b8165f10e8332c41b
Eric <ewd9627@rit.edu>
2019-06-03 15:57:33 -0400

    Uploading bare lwc for selecting an asset and displaying assets in table
    All combo boxes are hard coded and will soon be pulling from the database on Heroku

---
+++ salesforce/my-conservation-life/force-app/main/default/lwc/displayAssets/app.js
@@ -0,0 +1,22 @@
+import { LightningElement, track } from 'lwc';
+import fetchDataHelper from './fetchDataHelper';
+
+const columns = [
+    { label: 'Asset ID', fieldName: 'assetID' },
+    { label: 'Asset Type', fieldName: 'assetType'},
+    { label: 'Location', fieldName: 'location', type: 'location' },
+    { label: 'Funding Date', fieldName: 'fundDate', type: 'date' },
+    { label: 'Planting Date', fieldName: 'plantDate', type: 'date' },
+];
+
+export default class DatatableBasic extends LightningElement {
+    @track data = [];
+    @track columns = columns;
+    @track tableLoadingState = true;
+
+    async connectedCallback() {
+        const data = await fetchDataHelper({ amountOfRecords: 100 });
+        this.data = data;
+        this.tableLoadingState = false;
+    }
+}
