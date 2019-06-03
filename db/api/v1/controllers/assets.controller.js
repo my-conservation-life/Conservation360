48ee639559d53fc1b6a3fec0bae0614892b6bc62
James C. De Ricco <jcd7042@rit.edu>
2019-06-03 16:23:40 -0400

    assets.db: return undefined to let caller handle invalid projectId

--- db/api/v1/controllers/assets.controller.js
+++ db/api/v1/controllers/assets.controller.js
@@ -1,4 +1,5 @@
 const assetsDb = require('../db/assets.db');
+const { isValidDbInteger, DB_INTEGER_MAX } = require('../db');
 
 const find = async (req, res, next) => {
     const projectIdString = req.query['project_id'];
@@ -8,8 +9,9 @@ const find = async (req, res, next) => {
         if (projectIdString) {
             projectId = parseInt(projectIdString, 10);
 
-            if (isNaN(projectId)) {
-                throw new Error('Error while parsing the argument for project_id. Expected a number.');
+            if (isNaN(projectId) || !isValidDbInteger(projectId)) {
+                res.status(500).send(`Invalid argument for project_id. Expected a number between 1 and ${DB_INTEGER_MAX} but got '${projectIdString}'`);
+                return;
             }
         }
 
