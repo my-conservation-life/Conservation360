64e4d9126e6be3a8a9af1c81a2d3999dc4402b8f
Kirk Walek <kirk.walek@gmail.com>
2019-06-04 22:46:34 -0400

    choose and see properties for a type in create

--- salesforce/my-conservation-life/force-app/main/default/lwc/createAsset/createAsset.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/createAsset/createAsset.js
@@ -1,17 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import controllers from 'c/controllers';
 
 export default class CreateAsset extends LightningElement {
-    
-    get options() {
-        return [
-            { label: 'Baum', value: 'baum' },
-            { label: 'Eisbär', value: 'eisbär' },
-            { label: 'Eichhörnchen', value: 'eichhörnchen' },
-        ];
+
+    @track typeOptions
+    @track typeValue
+    @track descriptionOptions
+    @track descriptionValue
+    @track propertiesOptions
+    @track propertiesValue
+
+    // Fires when this component is inserted into the DOM
+    connectedCallback() {
+
+        controllers.assetDefinitions.getAll().then(assetDefinitions => {
+
+            // Must stringify because LWC must use primitives, no support for lists/objects
+            const definitionsString = JSON.stringify(assetDefinitions);
+
+            const types = [];
+            const descriptions = {};
+            const propertiesCollection = {};
+            if (definitionsString) {
+                const definitions = JSON.parse(definitionsString);
+                for(let definition of definitions) {
+                    const name = definition.assetType.name;
+                    const type = {
+                        label: name,
+                        value: name
+                    };
+                    types.push(type);
+                    descriptions[name] = definition.assetType.description;        
+                    propertiesCollection[name] = definition.properties;
+                }
+            }
+            this.typeOptions = types;
+            this.descriptionOptions = descriptions;
+            this.propertiesOptions = propertiesCollection;
+        });
     }
 
     handleChange(event) {
-        this.value = event.detail.value;
+        this.typeValue = event.detail.value;
+        this.descriptionValue = this.descriptionOptions[this.typeValue];
+        this.propertiesValue = this.propertiesOptions[this.typeValue];
     }
-
+    
 }
