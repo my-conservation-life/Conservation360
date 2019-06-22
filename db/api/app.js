eb106526f8fd854431ce1c8b434c3e56fb20d5d8
James C. De Ricco <jcd7042@rit.edu>
2019-06-21 20:46:14 -0400

    Refactor app.js and write basic integration test
    Separate DB and http server code out of app.js
    into db.js and server.js. Now starting up the app
    with "npm start" executes "node server.js".
    
    Since app.js now exports the Express "app" object,
    we can use this app object to do test endpoint API calls
    with supertest.
    
    DB configuration for the first integration test is currently in
    the beforeAll of the assets.test.js. We make this read
    test DB config from the .env file.

--- db/api/app.js
+++ db/api/app.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-console */
 
-const http = require('http');
 const express = require('express');
 const routes = {
     v1: require('./v1/routes')
 };
-const { Pool } = require('pg');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -22,37 +18,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(express.static('./client'));
-
-let dbConfig;
-if (process.env.DATABASE_UNIX_SOCKET_DIR) {
-    console.log('Configured database to connect using a local UNIX socket');
-    dbConfig = {
-        host: process.env.DATABASE_UNIX_SOCKET_DIR
-    };
-} else {
-    dbConfig = {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-    };
-}
-
-global.dbPool = new Pool(dbConfig);
-global.pool = global.dbPool;
-
 app.use('/api/v1', routes.v1);
 
-// Start server
-const server = http.createServer(app);
-server.listen(port, () => console.log('Listening on port', port));
-
-// Handle graceful server shutdown
-process.on('SIGTERM', () => {
-    console.log('SIGTERM received. Shutting down...');
-    server.close(() => {
-        console.log('HTTP server has shut down');
-        global.dbPool.end().then(() => {
-            console.log('PostgreSQL connections have shut down. This process will exit shortly...');
-            process.exitCode = 0;
-        });
-    });
-});
+module.exports = app;
---
+++ db/api/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+const http = require('http');
+const app = require('./app');
+const { initializeDbConnections } = require('./db');
+require('dotenv').config();
+
+initializeDbConnections();
+
+const port = process.env.PORT || 8080;
+
+// Start server
+const server = http.createServer(app);
+server.listen(port, () => console.log('Listening on port', port));
+
+// Handle graceful server shutdown
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received. Shutting down...');
+    server.close(() => {
+        console.log('HTTP server has shut down');
+        global.dbPool.end().then(() => {
+            console.log('PostgreSQL connections have shut down. This process will exit shortly...');
+            process.exitCode = 0;
+        });
+    });
+});
