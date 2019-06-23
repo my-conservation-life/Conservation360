534720d47be47aaf2907e5f95a19067a67d3ee04
Brian Picciano <BJPicciano@gmail.com>
2019-06-23 13:39:06 -0400

    Start test create asset definition property

--- salesforce/my-conservation-life/force-app/main/default/lwc/createAssetDefinitionProperty/__tests__/createAssetDefinitionProperty.test.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/createAssetDefinitionProperty/__tests__/createAssetDefinitionProperty.test.js
@@ -3,12 +3,26 @@ import CreateAssetDefinitionProperty from 'c/createAssetDefinitionProperty';
 
 describe('c-create-asset-definition-property', () => {
     let element;
+    let CONSOLE_WARN;
+
+    const attributes = {
+        name: 'testname',
+        data_type: 'testdatatype',
+        required: true,
+        is_private: true
+    };
+
+    beforeAll(() => {
+        CONSOLE_WARN = console.warn;
+        console.warn = () => { };
+    });
+
+    afterAll(() => {
+        console.warn = CONSOLE_WARN;
+    });
 
     beforeEach(() => {
-        element = createElement('c-map-project-assets', {
-            is: CreateAssetDefinitionProperty
-        });
-
+        element = createElement('c-map-project-assets', { is: CreateAssetDefinitionProperty });
         document.body.appendChild(element);
     });
 
@@ -19,7 +33,17 @@ describe('c-create-asset-definition-property', () => {
         }
     });
 
-    it('appends correctly', () => {
-        console.log(element.getAttributes());
+    it('gets correct attributes', () => {
+        const inputs = element.shadowRoot.querySelectorAll('lightning-input, lightning-combobox');
+        for (let input of inputs) {
+            const value = attributes[input.name];
+
+            if (input.type === 'checkbox') input.checked = value;
+            else input.value = value;
+
+            input.dispatchEvent(new Event('change'));
+        }
+
+        expect(element.getAttributes()).toEqual(attributes);
     });
 });
