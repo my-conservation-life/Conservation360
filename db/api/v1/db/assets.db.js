2857c0f24faae3d90f3de8d94d8bdecf8bd6da7e
Eric <ewd9627@rit.edu>
2019-06-24 18:07:28 -0400

    The good table, featuring names!

--- db/api/v1/db/assets.db.js
+++ db/api/v1/db/assets.db.js
@@ -1,19 +1,29 @@
-const QUERY_FIND =
-`SELECT id, project_id, asset_type_id, ST_X(location) AS latitude, ST_Y(location) AS longitude
-FROM asset`;
+const QUERY_FIND = `SELECT
+sponsor.name AS sponsor_name,
+project.name AS project_name,
+asset.id AS asset_id,
+asset_type.name AS asset_type,
+asset_type.description AS asset_description,
+ST_X(asset.location) AS latitude,
+ST_Y(asset.location) AS longitude
+FROM
+asset
+JOIN project ON asset.project_id = project.id
+JOIN sponsor ON project.sponsor_id = sponsor.id
+JOIN asset_type ON asset.asset_type_id = asset_type.id`;
 
 const QUERY_FIND_WHERE_PROJECT_ID = QUERY_FIND + ' WHERE project_id = $1';
 
 /**
  * Find project assets.
- * 
+ *
  * Returns all assets across all projects unless projectId is given.
- * 
+ *
  * @param {number} [projectId] - (assumes isValidDbInteger(projectId)): optional: filter assets by this Project ID
  * @returns {object[]|undefined} array of assets with fields (id, project_id, latitude, and longitude), or undefined if projectId is invalid.
  * @throws error if the DB query failed to execute
  */
-const find = async (projectId) => {
+const find = async projectId => {
     let query;
     let values;
 
--- salesforce/my-conservation-life/force-app/main/default/lwc/displayAssets/displayAssets.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/displayAssets/displayAssets.js
@@ -1,76 +1,85 @@
 import { LightningElement, wire, track } from 'lwc';
-import {assets, assetDefinitions} from 'c/controllers';
-import{CurrentPageReference} from 'lightning/navigation';
+import { assets } from 'c/controllers';
+import { CurrentPageReference } from 'lightning/navigation';
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
 
 //Columns currently in our data tables
 const columns = [
-    { label: 'Asset ID', fieldName: 'id', type: 'number'},
-    { label: 'Project ID', fieldName: 'project_id', type: 'number'},
-    { label: 'Asset Type', fieldName: 'asset_type_id', type: 'number'},
+    { label: 'Sponsor', fieldName: 'sponsor_name', type: 'string' },
+    { label: 'Project Name', fieldName: 'project_name', type: 'string' },
+    { label: 'Asset ID', fieldName: 'asset_id', type: 'number' },
+    { label: 'Asset Type', fieldName: 'asset_type', type: 'string' },
+    {
+        label: 'Asset Description',
+        fieldName: 'asset_description',
+        type: 'string'
+    },
     { label: 'Latitude', fieldName: 'latitude', type: 'number' },
-    { label: 'Longitude', fieldName: 'longitude', type: 'number'},
+    { label: 'Longitude', fieldName: 'longitude', type: 'number' }
 ];
 
 //Loads assets into the table through a database query, as found in the controllers
 export default class DisplayAssets extends LightningElement {
-    @wire(CurrentPageReference) pageRef;
-    @track selectedSponsor;
-    @track selectedProject;
-    @track selectedAsset;
-    sponsor_id = '';
-    project_id = 999;
-    asset_id = '';
-    @track data = [];
-    @track columns = columns;
-    @track tableLoadingState = true;
+  @wire(CurrentPageReference) pageRef;
+  @track selectedSponsor;
+  @track selectedProject;
+  @track selectedAsset;
+  sponsor_id = '';
+  project_id = 999;
+  asset_id = '';
+  @track data = [];
+  @track columns = columns;
+  @track tableLoadingState = true;
 
-    connectedCallback() {
-        registerListener('selectedSponsor', this.updateSponsor, this);
-        registerListener('selectedProject', this.updateProject, this);
-        registerListener('selectedAsset', this.updateAsset, this);
-        //Search for assets on a per-project basis
-        console.log('Listeners Registered');
+  connectedCallback() {
+      registerListener('selectedSponsor', this.updateSponsor, this);
+      registerListener('selectedProject', this.updateProject, this);
+      registerListener('selectedAsset', this.updateAsset, this);
+      //Search for assets on a per-project basis
+      console.log('Listeners Registered');
+  }
+  /**
+   *
+   * Update Sponsor, Project, and Asset
+   *
+   * @param {number} sponsor
+   * @param {number} project
+   * @param {number} asset
+   */
 
-    }
+  //Handle Sponsor Update
+  updateSponsor(sponsor) {
+      console.log(this.sponsor_id);
+      this.sponsor_id = sponsor;
+      this.updateTable();
+  }
+  //Handle Project Update
+  updateProject(project) {
+      console.log(this.project_id);
+      this.project_id = project;
+      this.updateTable();
+  }
+  //Handle Asset Update
+  updateAsset(asset) {
+      console.log(this.asset_id);
+      this.asset_id = asset;
+      this.updateTable();
+  }
 
-    /*
-    / Handle different query updates, each will update the table as needed
-    /
-    */
+  /**
+   * Update table with latest queries
+   */
+  updateTable() {
+      assets.find(parseInt(this.project_id, 10)).then(response => {
+          this.data = response;
+      });
+      //TODO: display text, numbers bad
 
-    //Handle Sponsor Update
-    updateSponsor(sponsor){
-        console.log(this.sponsor_id);
-        this.sponsor_id = sponsor;
-        this.updateTable();
-    }
-    //Handle Project Update
-    updateProject(project){
-        console.log(this.project_id);
-        this.project_id = project;
-        this.updateTable();
-    }
-    //Handle Asset Update
-    updateAsset(asset){
-        console.log(this.asset_id);
-        this.asset_id = asset;
-        this.updateTable();
-    }
+      this.tableLoadingState = false;
+      console.log('Updated Table');
+  }
 
-    /*
-    / Update table with newest queries
-    /
-    */
-    updateTable() {
-        assets.find(parseInt(this.project_id, 10)).then( response => { this.data = response; } );
-        //TODO: display text, numbers bad
-        
-        this.tableLoadingState = false;
-        console.log('Updated Table');
-    }
-
-    disconnectedCallback() {
-        unregisterAllListeners(this);
-    }
+  disconnectedCallback() {
+      unregisterAllListeners(this);
+  }
 }
