56450b004d9910bf43d7534a3f2cb4b9d4d0d72f
Eric <ewd9627@rit.edu>
2019-06-25 18:23:11 -0400

    Seeing if "value" was required...

--- salesforce/my-conservation-life/force-app/main/default/lwc/selectAsset/selectAsset.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/selectAsset/selectAsset.js
@@ -1,69 +1,73 @@
 import { LightningElement, wire, track } from 'lwc';
-import {CurrentPageReference} from 'lightning/navigation';
+import { CurrentPageReference } from 'lightning/navigation';
 import { fireEvent } from 'c/pubsub';
 
 export default class App extends LightningElement {
-    @wire(CurrentPageReference) pageRef;
-    @track
-    state = {
-        title: 'Select Asset Type',
-    };
-    @track
-    sponsor;
-    get sponsors() {
-        return [
-            {
-                label: 'Default: Any',
-                value: '0',
-            },
-            {
-                label: 'Seneca Park Zoo',
-                value: '1',
-            }
-        ];
-    }
-    @track
-    project;
-    get projects() {
-        return [
-            {
-                label: 'Default: Any',
-                value: '0',
-            },
-            {
-                label: 'Madagascar Reforesting Project',
-                value: '1',
-            },
-            {
-                label: 'Save The Great Barrier Reef',
-                value: '2',
-            }
-        ];
-    }
-    @track
-    asset;
-    get assets() {
-        return [
-            {
-                label: 'Default: Any',
-                value: '0',
-            },
-            {
-                label: 'Tree',
-                value: '1',
-            }
-        ];
-    }
-    handleSponsorChange(event) {
-        this.sponsor = event.detail.value;
-        fireEvent(this.pageRef, 'selectedSponsor', this.sponsor);
-    }
-    handleProjectChange(event) {
-        this.project = event.detail.value;
-        fireEvent(this.pageRef, 'selectedProject', this.project);
-    }
-    handleAssetChange(event) {
-        this.asset = event.detail.value;
-        fireEvent(this.pageRef, 'selectedAsset', this.asset);
-    }
+  @wire(CurrentPageReference) pageRef;
+  @track
+  state = {
+      title: 'Select Asset Type'
+  };
+  @track
+  sponsor;
+  get sponsors() {
+      return [
+          {
+              label: 'Default: Any',
+              value: ''
+          },
+          {
+              label: 'Seneca Park Zoo',
+              value: '1'
+          }
+      ];
+  }
+  @track
+  project;
+  get projects() {
+      return [
+          {
+              label: 'Default: Any',
+              value: ''
+          },
+          {
+              label: 'Madagascar Reforesting Project',
+              value: '1'
+          },
+          {
+              label: 'ID=2',
+              value: '2'
+          }
+      ];
+  }
+  @track
+  asset;
+  get assets() {
+      return [
+          {
+              label: 'Default: Any',
+              value: ''
+          },
+          {
+              label: 'Tree',
+              value: '1'
+          },
+          {
+              label: 'ID=2',
+              value: '2'
+          }
+      ];
+  }
+  handleSponsorChange(event) {
+      this.sponsor = event.detail.value;
+      fireEvent(this.pageRef, 'selectedSponsor', this.sponsor);
+  }
+  handleProjectChange(event) {
+      this.project = event.detail.value;
+      fireEvent(this.pageRef, 'selectedProject', this.project);
+  }
+  handleAssetChange(event) {
+      this.asset = event.detail.value;
+      fireEvent(this.pageRef, 'selectedAsset', this.asset);
+  }
 }
