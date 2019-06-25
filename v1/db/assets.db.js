c57d26c7ed7d14d21c50a5f7ccfcc192137842fb
Eric <ewd9627@rit.edu>
2019-06-25 19:56:16 -0400

    Trying something new

--- v1/db/assets.db.js
+++ v1/db/assets.db.js
@@ -10,7 +10,8 @@ FROM
 asset
 JOIN project ON asset.project_id = project.id
 JOIN sponsor ON project.sponsor_id = sponsor.id
-JOIN asset_type ON asset.asset_type_id = asset_type.id`;
+JOIN asset_type ON asset.asset_type_id = asset_type.id
+WHERE TRUE `;
 
 const QUERY_FIND_WHERE =
   QUERY_FIND +
@@ -25,36 +26,53 @@ const QUERY_FIND_WHERE =
  * @returns {object[]|undefined} array of assets with fields (id, project_id, latitude, and longitude), or undefined if projectId is invalid.
  * @throws error if the DB query failed to execute
  */
+
+const find = async (sponsorId, projectId, assetType) => {
+    let query = QUERY_FIND;
+    let criteria;
+    if (typeof sponsorId !== 'undefined') {
+        criteria = criteria + 'AND sponsor_id = ' + sponsorId + ' ';
+    }
+    if (typeof projectId !== 'undefined') {
+        criteria = criteria + 'AND project_id = ' + projectId + ' ';
+    }
+    if (typeof assetType !== 'undefined') {
+        criteria = criteria + 'AND asset_type_id = ' + assetType + ' ';
+    }
+    const result = await global.dbPool.query(query + criteria);
+    return result.rows;
+};
+/**
 const find = async (sponsorId, projectId, assetType) => {
     let query = QUERY_FIND_WHERE;
     let values = [];
 
     if (typeof sponsorId !== 'undefined') {
         if (sponsorId == 0) {
-            values += 'TRUE';
+            values += '';
         } else values += sponsorId;
     } else if (typeof sponsorId == 'undefined') {
-        values += 'TRUE';
+        values += '';
     }
     if (typeof projectId !== 'undefined') {
         if (projectId == 0) {
-            values += 'TRUE';
+            values += '';
         } else values += projectId;
     } else if (typeof projectId == 'undefined') {
-        values += 'TRUE';
+        values += '';
     }
     if (typeof assetType !== 'undefined') {
         if (assetType == 0) {
-            values += 'TRUE';
+            values += '';
         } else values += assetType;
     } else if (typeof assetType == 'undefined') {
-        values += 'TRUE';
+        values += '';
     }
 
     const result = await global.dbPool.query(query, values);
     return result.rows;
 };
-
+*/
 module.exports = {
     find
 };
