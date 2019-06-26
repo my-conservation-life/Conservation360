2c59c97aacf80bfd1039bc55aca5ca893406f814
Eric <ewd9627@rit.edu>
2019-06-26 19:25:07 -0400

    magic

--- db/api/v1/controllers/assets.controller.js
+++ db/api/v1/controllers/assets.controller.js
@@ -1,10 +1,12 @@
 const assetsDb = require('../db/assets.db');
 
 const find = async (req, res, next) => {
+    const sponsorId = req.query.sponsor_id;
     const projectId = req.valid.project_id;
+    const assetId = req.query.asset_type_id;
 
     try {
-        const assets = await assetsDb.find(projectId);
+        const assets = await assetsDb.find(sponsorId, projectId, assetId);
         res.json(assets);
     } catch (error) {
         next(error);
