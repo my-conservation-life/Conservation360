19557e40b52ad8d918e1e5c20ab0d3a5357dc713
James C. De Ricco <jcd7042@rit.edu>
2019-06-29 18:32:21 -0400

    mapProjectAssets.utils: markerFromAsset: improve JSDoc
    Document the shape of the parameter object.
    Document the return value of the function.

--- salesforce/my-conservation-life/force-app/main/default/lwc/mapProjectAssets/utils.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/mapProjectAssets/utils.js
@@ -4,6 +4,10 @@
 /**
  * Returns a Leaflet marker at the location of the asset
  * 
- * precondition: L is an initialized leaflet object
+ * @param {Object} asset - a MyConservationLife asset
+ * @param {number} asset.latitude - Latitude of the asset
+ * @param {number} asset.longitude - Longitude of the asset
+ * 
+ * @returns {L.Marker} a Leaflet marker for the location of the given asset
  */
 export const markerFromAsset = (asset) => L.marker(L.latLng(asset.latitude, asset.longitude));
