32f686599ce41e0e067c2cde140e63b3d37950d9
Eric <ewd9627@rit.edu>
2019-07-03 15:25:17 -0400

    attempt at pushing schema to DB

--- db/api/tests/endpoints/assets.test.js
+++ db/api/tests/endpoints/assets.test.js
@@ -10,6 +10,15 @@ describe('GET assets', () => {
         const dbConfig = {
             host: process.env.DATABASE_UNIX_SOCKET_DIR
         };
+        /* var fs = require('fs');
+        console.log('reading schema');
+        var schema = fs.readFileSync('../schema/schema.sql', {
+            encoding: 'utf8'
+        });
+        console.log(schema);
+        console.log('sending schema');
+        global.dbPool.query(schema);
+        console.log('schema sent'); */
 
         global.dbPool = new Pool(dbConfig);
         global.pool = global.dbPool;
@@ -19,7 +28,7 @@ describe('GET assets', () => {
         global.dbPool.end();
     });
 
-    it('returns HTTP 200 response', (done) => {
+    it('returns HTTP 200 response', done => {
         request(app)
             .get('/api/v1/assets')
             .end((err, res) => {
