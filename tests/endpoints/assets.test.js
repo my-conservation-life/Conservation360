3a98f517c2402bac25c0071195016870b8b6aa2b
Eric <ewd9627@rit.edu>
2019-07-03 19:45:23 -0400

    Separation of concern, add setup.js for testing

--- tests/endpoints/assets.test.js
+++ tests/endpoints/assets.test.js
@@ -1,31 +1,14 @@
 const request = require('supertest');
-const { Pool } = require('pg');
 const app = require('../../app');
-
+const { setup, teardown, loadSQL } = require('../setup');
 describe('GET assets', () => {
     beforeAll(() => {
-        require('dotenv').config();
-
-        // ENTER YOUR TEST DB CONNECTION CONFIG HERE
-        const dbConfig = {
-            host: process.env.DATABASE_UNIX_SOCKET_DIR
-        };
-        /* var fs = require('fs');
-        console.log('reading schema');
-        var schema = fs.readFileSync('../schema/schema.sql', {
-            encoding: 'utf8'
-        });
-        console.log(schema);
-        console.log('sending schema');
-        global.dbPool.query(schema);
-        console.log('schema sent'); */
-
-        global.dbPool = new Pool(dbConfig);
-        global.pool = global.dbPool;
+        setup();
+        loadSQL('../schema/sample-data-1.sql');
     });
 
     afterAll(() => {
-        global.dbPool.end();
+        teardown();
     });
 
     it('returns HTTP 200 response', done => {
