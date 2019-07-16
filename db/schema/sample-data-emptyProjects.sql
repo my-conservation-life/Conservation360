314ec102df94ddfb058ad6e76210b8f10086dbfc
James C. De Ricco <jcd7042@rit.edu>
2019-07-15 21:44:42 -0400

    test /bbox-assets: create tests and sample-data-emptyProjects.sql

---
+++ db/schema/sample-data-emptyProjects.sql
@@ -0,0 +1,8 @@
+INSERT INTO sponsor (id, name) VALUES (1, 'Seneca Park Zoo');
+INSERT INTO sponsor (id, name) VALUES (2, 'Bronx Zoo');
+INSERT INTO project (id, sponsor_id, name) VALUES (1, 1, 'Madagascar Reforesting Project');
+INSERT INTO project (id, sponsor_id, name) VALUES (2, 1, 'Lemur Protection');
+INSERT INTO project (id, sponsor_id, name) VALUES (3, 2, 'Bison Protection');
+INSERT INTO asset_type (id, name) VALUES (1, 'Tree');
+INSERT INTO asset_type (id, name) VALUES (2, 'Lemur');
+INSERT INTO asset_type (id, name) VALUES (3, 'Bison');
