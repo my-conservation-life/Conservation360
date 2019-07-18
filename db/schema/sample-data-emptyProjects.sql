097c463a32a2532f215bda3dadc9f0f7189dbbbc
Kirk Walek <kirk.walek@gmail.com>
2019-07-17 20:48:58 -0400

    test for get assetdefinitions

--- db/schema/sample-data-emptyProjects.sql
+++ db/schema/sample-data-emptyProjects.sql
@@ -1,8 +1,8 @@
-INSERT INTO sponsor (id, name) VALUES (1, 'Seneca Park Zoo');
-INSERT INTO sponsor (id, name) VALUES (2, 'Bronx Zoo');
-INSERT INTO project (id, sponsor_id, name) VALUES (1, 1, 'Madagascar Reforesting Project');
-INSERT INTO project (id, sponsor_id, name) VALUES (2, 1, 'Lemur Protection');
-INSERT INTO project (id, sponsor_id, name) VALUES (3, 2, 'Bison Protection');
-INSERT INTO asset_type (id, name) VALUES (1, 'Tree');
-INSERT INTO asset_type (id, name) VALUES (2, 'Lemur');
-INSERT INTO asset_type (id, name) VALUES (3, 'Bison');
+INSERT INTO sponsor (name) VALUES ('Seneca Park Zoo');
+INSERT INTO sponsor (name) VALUES ('Bronx Zoo');
+INSERT INTO project (sponsor_id, name) VALUES (1, 'Madagascar Reforesting Project');
+INSERT INTO project (sponsor_id, name) VALUES (1, 'Lemur Protection');
+INSERT INTO project (sponsor_id, name) VALUES (2, 'Bison Protection');
+INSERT INTO asset_type (name) VALUES ('Tree');
+INSERT INTO asset_type (name) VALUES ('Lemur');
+INSERT INTO asset_type (name) VALUES ('Bison');
