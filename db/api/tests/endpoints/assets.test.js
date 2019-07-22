9d5e8a9fa0c2e7b2c472274a88c46dc4d5dac400
Eric <ewd9627@rit.edu>
2019-07-22 16:41:11 -0400

    Implement test for checking assets from DB

--- db/api/tests/endpoints/assets.test.js
+++ db/api/tests/endpoints/assets.test.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
-
 const app = require('../../app');
-const { setup, teardown } = require('../setup');
+const { setup, teardown, loadSQL } = require('../setup');
 
 describe('GET assets', () => {
     beforeAll(async () => {
@@ -12,12 +11,37 @@ describe('GET assets', () => {
         await teardown();
     });
 
+    function contains(arr, key, val) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i][key] === val) return true;
+        }
+        return false;
+    }
+
     it('returns HTTP 200 response', () => {
         return request(app)
             .get('/api/v1/assets')
             .then(res => {
                 expect(res.status).toBe(200);
                 expect(res.text).toBe('[]');
+            });
+    });
+
+    it('returns assets with expected types', done => {
+        loadSQL('../schema/sample-data-1.sql');
+        request(app)
+            .get('/api/v1/assets')
+            .expect(200)
+            .then(response => {
+                // response.body should contain the id of the created data_type
+                const data = response.body;
+                expect(data).toBeTruthy();
+                expect(typeof data).toBe('object');
+                expect(contains(data, 'sponsor_name', 'Bronx Zoo'));
+                expect(contains(data, 'sponsor_name', 'Seneca Park Zoo'));
+                expect(contains(data, 'asset_id', '21'));
+                expect(contains(data, 'asset_type', 'Bison'));
+                done();
             });
     });
 });
