7e3a43af7ededcbd28a2ab13dcb2ad21e714158f
Kirk Walek <kirk.walek@gmail.com>
2019-07-24 20:29:45 -0400

    displayed a list of access codes

--- salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
@@ -1,16 +1,19 @@
 import { LightningElement, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getListUi } from 'lightning/uiListApi';
  
-//import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import ACCESS_CODE_OBJECT from '@salesforce/schema/Access_Code__c';
 import NAME_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
  
 export default class GenerateAssetCode extends LightningElement {
-    @wire(getRecord, {
-        recordId: "a003i000001TkwtAAC",
-        fields: [NAME_FIELD]
+    @wire(getListUi, {
+        objectApiName: ACCESS_CODE_OBJECT,
+        listViewApiName: 'All_Access_Codes',
+        sortBy: NAME_FIELD,
+        pageSize: 100
     })
-    listView({ error, data }) {
-        console.log(error, data);
+    listView;
+ 
+    get asset_codes() {
+        return this.listView.data.records.records;
     }
-    
 }
