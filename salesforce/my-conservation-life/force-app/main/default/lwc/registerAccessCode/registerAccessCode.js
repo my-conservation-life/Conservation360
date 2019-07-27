855d044739acf72d93f9dad357842435de4e8028
Brian Picciano <BJPicciano@gmail.com>
2019-07-27 19:27:06 -0400

    Can register access codes!

--- salesforce/my-conservation-life/force-app/main/default/lwc/registerAccessCode/registerAccessCode.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/registerAccessCode/registerAccessCode.js
@@ -1,27 +1,64 @@
-import { LightningElement, wire } from 'lwc';
-import { getRecord, updateRecord } from 'lightning/uiRecordApi';
+/* eslint-disable @lwc/lwc/no-async-operation */
+import { LightningElement, track } from 'lwc';
+import { updateRecord } from 'lightning/uiRecordApi';
 
-// import ACCESS_CODE_OBJECT from '@salesforce/schema/Access_Code__c';
+import USER_ID from '@salesforce/user/Id';
 import ID_FIELD from '@salesforce/schema/Access_Code__c.Id';
-// import DONOR_FIELD from '@salesforce/schema/Access_Code__c.Donor__c';
-import ASSET_ID_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
-
+import DONOR_FIELD from '@salesforce/schema/Access_Code__c.Donor__c';
 
 export default class RegisterAccessCode extends LightningElement {
-    renderedCallback() {
+    MESSAGE_TIMEOUT = 5500;
+    accessCode;
+
+    @track hasError = false;
+    @track hasSuccess = false;
+
+    registerAccessCode() {
+        if (!this.accessCode) this.showError();
+
         const fields = {};
-        // fields[DONOR_FIELD.fieldApiName] = this.contactId;
-        fields[ID_FIELD.fieldApiName] = 'a003i000001Tkx3AAC';
-        fields[ASSET_ID_FIELD.fieldApiName] = 555;
+        fields[ID_FIELD.fieldApiName] = this.accessCode;
+        fields[DONOR_FIELD.fieldApiName] = USER_ID;
 
-        const recordInput = { fields };
-
-        updateRecord(recordInput)
-            .then((response) => {
-                console.log(response);
+        updateRecord({ fields })
+            .then(() => {
+                this.showSuccess();
             })
             .catch(error => {
-                console.log(error);
+                this.showError();
+                console.error(error);
             });
+    }
+
+
+    /**
+     * Event handler to save the user inputted value to this property's attributes.
+     * 
+     * @param {Event} e - a change event dispatched by each input
+     */
+    saveAttribute(e) {
+        const ele = e.srcElement;
+        const name = ele.name;
+        const value = ele.value;
+
+        this[name] = value;
+    }
+
+    showSuccess() {
+        this.hasError = false;
+        this.hasSuccess = true;
+
+        setTimeout(() => {
+            this.hasSuccess = false;
+        }, this.MESSAGE_TIMEOUT);
+    }
+
+    showError() {
+        this.hasError = true;
+        this.hasSuccess = false;
+
+        setTimeout(() => {
+            this.hasError = false;
+        }, this.MESSAGE_TIMEOUT);
     }
 }
