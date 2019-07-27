36ce6c2d64fc7882224b72db5a27d8a7b971f2fd
Brian Picciano <BJPicciano@gmail.com>
2019-07-27 18:47:15 -0400

    Updates asset code's asset id

--- salesforce/my-conservation-life/force-app/main/default/lwc/registerAccessCode/registerAccessCode.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/registerAccessCode/registerAccessCode.js
@@ -1,12 +1,27 @@
 import { LightningElement, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getRecord, updateRecord } from 'lightning/uiRecordApi';
 
-import API_URL_FIELD from '@salesforce/schema/MCL_CONFIG__mdt.API_URL__c';
+// import ACCESS_CODE_OBJECT from '@salesforce/schema/Access_Code__c';
+import ID_FIELD from '@salesforce/schema/Access_Code__c.Id';
+// import DONOR_FIELD from '@salesforce/schema/Access_Code__c.Donor__c';
+import ASSET_ID_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
+
 
 export default class RegisterAccessCode extends LightningElement {
-    @wire(getRecord, { recordId: 'MCL_Config', fields: [API_URL_FIELD] })
-    mclConfig({ error, data }) {
-        if (error) console.error(error.body);
-        if (data) console.log('===', data);
+    renderedCallback() {
+        const fields = {};
+        // fields[DONOR_FIELD.fieldApiName] = this.contactId;
+        fields[ID_FIELD.fieldApiName] = 'a003i000001Tkx3AAC';
+        fields[ASSET_ID_FIELD.fieldApiName] = 555;
+
+        const recordInput = { fields };
+
+        updateRecord(recordInput)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 }
