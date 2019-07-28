3a0751d9dd85aec13b8d3933479e5b7d727c0f9f
Kirk Walek <kirk.walek@gmail.com>
2019-07-28 12:40:20 -0400

    Now displays the code on creation

--- salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
@@ -1,21 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
  
 import ACCESS_CODE_OBJECT from '@salesforce/schema/Access_Code__c';
 import ID_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
 
+function getOpenAsset() {
+
+
+
+    return 1;
+}
+
 export default class GenerateAssetCode extends LightningElement {
+
+    @track code
 
     generateCode() {
         const fields = {};
-        fields[ID_FIELD.fieldApiName] = 1;
+        fields[ID_FIELD.fieldApiName] = getOpenAsset();
         const recordInput = { apiName: ACCESS_CODE_OBJECT.objectApiName, fields };
         createRecord(recordInput)
             .then(assetCode => {
-                console.log(assetCode);
+                this.code = assetCode.id;
             })
-            .catch(error => {
-                console.log(error);
+            .catch(() => {
+                this.code = 'Error: code not generated';
             });
     }
 
