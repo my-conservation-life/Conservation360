e20ebacd9a71ce161401d7340cbbd78031b3b6d7
Kirk Walek <kirk.walek@gmail.com>
2019-07-28 12:03:47 -0400

    able to create new asseet codes from lwc

--- salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
@@ -1,19 +1,22 @@
-import { LightningElement, wire } from 'lwc';
-import { getListUi } from 'lightning/uiListApi';
+import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
  
 import ACCESS_CODE_OBJECT from '@salesforce/schema/Access_Code__c';
-import NAME_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
- 
+import ID_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
+
 export default class GenerateAssetCode extends LightningElement {
-    @wire(getListUi, {
-        objectApiName: ACCESS_CODE_OBJECT,
-        listViewApiName: 'All_Access_Codes',
-        sortBy: NAME_FIELD,
-        pageSize: 100
-    })
-    listView;
- 
-    get asset_codes() {
-        return this.listView.data.records.records;
+
+    generateCode() {
+        const fields = {};
+        fields[ID_FIELD.fieldApiName] = 1;
+        const recordInput = { apiName: ACCESS_CODE_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then(assetCode => {
+                console.log(assetCode);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
+
 }
