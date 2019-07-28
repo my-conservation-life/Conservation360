70a085b3d242d1cd7b5fa42d820bc64157e6d3c7
Kirk Walek <kirk.walek@gmail.com>
2019-07-28 13:29:02 -0400

    concept started asset_id search

--- salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
+++ salesforce/my-conservation-life/force-app/main/default/lwc/generateAssetCode/generateAssetCode.js
@@ -1,15 +1,9 @@
 import { LightningElement, track } from 'lwc';
+import { assets } from 'c/controllers';
 import { createRecord } from 'lightning/uiRecordApi';
  
 import ACCESS_CODE_OBJECT from '@salesforce/schema/Access_Code__c';
 import ID_FIELD from '@salesforce/schema/Access_Code__c.Asset_Id__c';
-
-function getOpenAsset() {
-
-
-
-    return 1;
-}
 
 export default class GenerateAssetCode extends LightningElement {
 
@@ -17,15 +11,34 @@ export default class GenerateAssetCode extends LightningElement {
 
     generateCode() {
         const fields = {};
-        fields[ID_FIELD.fieldApiName] = getOpenAsset();
         const recordInput = { apiName: ACCESS_CODE_OBJECT.objectApiName, fields };
-        createRecord(recordInput)
-            .then(assetCode => {
-                this.code = assetCode.id;
-            })
-            .catch(() => {
-                this.code = 'Error: code not generated';
+        this.code = 'Generating code...';
+        
+        assets.find().then( assetObjects => {
+            var assetId = -1;
+            var i;
+            var map = {};
+
+            assetObjects.forEach( assetObject => {
+                i = parseInt(assetObject.asset_id, 10);
+                map[i] = 1;
             });
+
+            for (i = 1; i <= (2^63)*2 ; i++) {
+                if (map[i] === undefined) {
+                    assetId = i;
+                    break;
+                }
+            }
+
+            fields[ID_FIELD.fieldApiName] = assetId;
+
+            createRecord(recordInput).then(assetCode => {
+                this.code = assetCode.id;
+            });
+        }).catch(() => {
+            this.code = 'Error: code not generated';
+        });
     }
 
 }
