7ca3f9f609614cf4431f5a8858bab1528823040a
Nikolas Tilley <nmt2400@g.rit.edu>
2019-10-17 14:37:25 -0400

    Added controller tests for project find
    All project find tests pass

--- db/api/v1/controllers/__tests__/projects.controller.test.js
+++ db/api/v1/controllers/__tests__/projects.controller.test.js
@@ -35,24 +35,66 @@ describe('projects.controller.find', () => {
         projectsDb.find = jest.fn(async () => expected_projects);
     });
 
-    it('TODO: accesses DB and sends JSON response when no project_id is provided', async () => {
-        expect(false).toBeTruthy();
+    it('accesses DB and sends JSON response when no parameters are provided', async () => {
+        await find(req, res, next);
+        expect(projectsDb.find).toHaveBeenCalledWith(undefined, undefined, undefined);
+        expect(res.json).toHaveBeenCalledWith(expected_projects);
     });
 
-    it('TODO: accesses DB and sends JSON response when project_id is provided', async () => {
-        expect(false).toBeTruthy();
-    });
-
-    it('TODO: accesses DB and sends JSON response when project name is provided', async () => {
-        expect(false).toBeTruthy();
+    it('accesses DB and sends JSON response when project id is provided', async () => {
+        req.valid['id'] = 2;
+        await find(req, res, next);
+        expect(projectsDb.find).toHaveBeenCalledWith(2, undefined, undefined);
+        expect(res.json).toHaveBeenCalledWith(expected_projects);
     });
 
     it('TODO: accesses DB and sends JSON response when sponsor_id is provided', async () => {
-        expect(false).toBeTruthy();
+        req.valid['sponsor_id'] = 8;
+        await find(req, res, next);
+        expect(projectsDb.find).toHaveBeenCalledWith(undefined, 8, undefined);
+        expect(res.json).toHaveBeenCalledWith(expected_projects);
     });
 
-    it('TODO: ignores unvalidated sponsor_id', async () => {
-        expect(false).toBeTruthy();
+    it('accesses DB and sends JSON response when project name is provided', async () => {
+        req.valid['name'] = 'Madagascar Reforestation';
+        await find(req, res, next);
+        expect(projectsDb.find).toHaveBeenCalledWith(undefined, undefined, 'Madagascar Reforestation');
+        expect(res.json).toHaveBeenCalledWith(expected_projects);
+    });
+
+    it('accesses DB and sends JSON response when sponsor_id and name is provided', async () => {
+        req.valid['sponsor_id'] = 8;
+        req.valid['name'] = 'Madagascar Reforestation';
+        await find(req, res, next);
+        expect(projectsDb.find).toHaveBeenCalledWith(undefined, 8, 'Madagascar Reforestation');
+        expect(res.json).toHaveBeenCalledWith(expected_projects);    });
+
+    it('ignores unvalidated id', async () => {
+        const getProjectId = jest.fn(() => 'a');
+        Object.defineProperty(req.query, 'id', { get: getProjectId });
+        await find(req, res, next);
+        expect(getProjectId).not.toHaveBeenCalled();    
+    });
+
+    it('ignores unvalidated sponsor_id', async () => {
+        const getSponsorId = jest.fn(() => 'a');
+        Object.defineProperty(req.query, 'sponsor_id', { get: getSponsorId });
+        await find(req, res, next);
+        expect(getSponsorId).not.toHaveBeenCalled();   
+    });
+
+    it('ignores null name', async () => {
+        const getName = jest.fn(() => null);
+        Object.defineProperty(req.query, 'name', { get: getName });
+        await find(req, res, next);
+        expect(getName).not.toHaveBeenCalled();  
+    });
+
+    it('ignores empty name', async () => {
+        const getName = jest.fn(() => '');
+        Object.defineProperty(req.query, 'name', { get: getName });
+        await find(req, res, next);
+        expect(getName).not.toHaveBeenCalled(); 
     });
 });
 
