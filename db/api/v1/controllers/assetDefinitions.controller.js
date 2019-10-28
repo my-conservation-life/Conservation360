c9f58f760824efefb4f608c74da8d08f26d8d19b
Jared <jed.jasie@gmail.com>
2019-10-28 15:50:25 -0400

    BREAKING CHANGE, DO NOT MERGE

--- db/api/v1/controllers/assetDefinitions.controller.js
+++ db/api/v1/controllers/assetDefinitions.controller.js
@@ -1,28 +1,28 @@
-const db = require('../db');
-
+st db = require('../db');
+asdfasdfasdf asdf 
 const find = async (req, res, next) => {
-
+asdfasdf asdf adsf 
     const predicates = req.query;
 
     try {
-        const assetDefinitions = await db.assetDefinitions.find(predicates);
+        const assetDefinitions = await db.asseasdfasdfasdtDefinitions.find(predicates);
         res.json(assetDefinitions);
     } catch (e) {
         next(e);
-    }
+    }fasdfa
 };
 
 const create = async (req, res, next) => {
     const assetDefinition = req.valid.assetDefinition;
-    try {
+    try {dfasasdfasd
         const assetTypeId = await db.assetDefinitions.create(assetDefinition);
         res.json(assetTypeId);
     } catch (e) {
         next(e);
     }
 };
-
+asdfasdf
 module.exports = {
     find,
-    create
+    creasdfasdfate
 };
