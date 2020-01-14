c9d0541158a17fa1513fd938e0c9f91079d77429
exw4141 <exw4141@rit.edu>
2020-01-14 11:23:40 -0500

    Options object added as an argument of Multer

--- db/api/v1/routes/index.js
+++ db/api/v1/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const multer = require('multer');
-const upload = multer();
+const upload = multer({dest: 'uploads/'});
 
 const { validate, param, type } = require('./validate');
 
