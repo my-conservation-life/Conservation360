9aba767d35a797ba9e647f71496c3712a4392f4f
exw4141 <exw4141@rit.edu>
2020-01-15 03:47:27 -0500

    Configure Express to enable pre-flighting

--- db/api/app.js
+++ db/api/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 const express = require('express');
+const cors = require('cors');
 const routes = {
     v1: require('./v1/routes')
 };
@@ -8,6 +9,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.options('*', cors());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
