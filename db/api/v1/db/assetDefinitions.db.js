b4c3ea16a539f301b5cd62c4bd9bba4d7e4f6431
exw4141 <exw4141@rit.edu>
2020-01-21 18:38:38 -0500

    Look at properties object

--- db/api/v1/db/assetDefinitions.db.js
+++ db/api/v1/db/assetDefinitions.db.js
@@ -189,7 +189,7 @@ const storeCSV = async(assetTypeId, csvJson) => {
             }
         }
     }
-    return(propertyToAdd);
+    return(properties);
 };
 
 const updateProperty = async(assetId, assetTypeId, newValue) => {
