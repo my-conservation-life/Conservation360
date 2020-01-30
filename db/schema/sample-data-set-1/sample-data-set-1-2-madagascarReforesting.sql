59d3e56fcae4b6e9e611522b8372b5929ddf2ab4
Nikolas Tilley <nmt2400@g.rit.edu>
2020-01-30 18:04:06 -0500

    Fixing Sample Data
    Fixed lat/lon orders so that assets appear where they were intended.
    
    Renamed Seneca Park Zoo to Seneca Park Zoo Society

--- db/schema/sample-data-set-1/sample-data-set-1-2-madagascarReforesting.sql
+++ db/schema/sample-data-set-1/sample-data-set-1-2-madagascarReforesting.sql
@@ -3,5 +3,5 @@ INSERT INTO project (sponsor_id, name) VALUES (1, 'Madagascar Reforesting');
 INSERT INTO asset_type (name) VALUES ('Tree');
 
 INSERT INTO asset (project_id, asset_type_id, location)
-SELECT 2, 4, ST_MakePoint(random_between(-16.8, -17.5), random_between(46, 47))
+SELECT 2, 4, ST_MakePoint(random_between(46, 47), random_between(-16.8, -17.5))
 FROM generate_series(1, 20);
