c7233d40265a68e7cc6f10a0d5edc7350fd01417
tilley14 <nmt2400@g.rit.edu>
2020-02-03 16:20:19 -0500

    Fixing Sample Data (#68)
    Fixed lat/lon orders so that assets appear where they were intended.
    
    Renamed Seneca Park Zoo to Seneca Park Zoo Society

--- db/schema/sample-data-1.sql
+++ db/schema/sample-data-1.sql
@@ -1,4 +1,4 @@
-INSERT INTO sponsor (id, name) VALUES (1, 'Seneca Park Zoo');
+INSERT INTO sponsor (id, name) VALUES (1, 'Seneca Park Zoo Society');
 INSERT INTO sponsor (id, name) VALUES (2, 'Bronx Zoo');
 INSERT INTO project (id, sponsor_id, name) VALUES (1, 1, 'Madagascar Reforesting Project');
 INSERT INTO project (id, sponsor_id, name) VALUES (2, 1, 'Lemur Protection');
@@ -8,11 +8,11 @@ INSERT INTO asset_type (id, name) VALUES (2, 'Lemur');
 INSERT INTO asset_type (id, name) VALUES (3, 'Bison');
 
 INSERT INTO asset (project_id, asset_type_id, location)
-	SELECT 1, 1, ST_MakePoint(-19 + ((random() - 0.5) * 4), 46 + ((random() - 0.5) * 4))
+	SELECT 1, 1, ST_MakePoint(46 + ((random() - 0.5) * 4), -19 + ((random() - 0.5) * 4))
 	FROM generate_series(1, 10);
 
 INSERT INTO asset (project_id, asset_type_id, location)
-	SELECT 2, 2, ST_MakePoint(-19.8 + ((random() - 0.5) * 4), 46 + ((random() - 0.5) * 4))
+	SELECT 2, 2, ST_MakePoint(46 + ((random() - 0.5) * 4), -19.8 + ((random() - 0.5) * 4))
 	FROM generate_series(1, 10);
 
 INSERT INTO asset (project_id, asset_type_id, location)
