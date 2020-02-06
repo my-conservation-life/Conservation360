557f66b5e768e3d4e8a9f679399ab40b561976db
exw4141 <exw4141@rit.edu>
2020-02-06 14:43:26 -0500

    Remove empty line

--- db/api/app.js
+++ db/api/app.js
@@ -12,7 +12,6 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
     next();
 });
 app.use(bodyParser.urlencoded({
