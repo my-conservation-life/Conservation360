91e7ef5017035d179192adfe9c93dd6a9f2cb00c
Nikolas Tilley <nmt2400@g.rit.edu>
2020-02-13 17:45:19 -0500

    debugging geometry

--- db/api/v1/db/temporal.db.js
+++ db/api/v1/db/temporal.db.js
@@ -117,13 +117,13 @@ const temporalSearch = async (geometry, asset_id, sponsor_id, project_id, asset_
 
     var temporal_results = [];
     // Should be ordered by asset id then ordered by date
-    var last_asset_id = -1;
+    var last_asset_id = '';
     var last_asset_date = '';
     var row = {};
     for (var i = 0; i < result.rows.length; i++) {
         row = result.rows[i];
         // Start a new asset record
-        if (row.asset_id != last_asset_id || row.date != last_asset_date) {
+        if (row.asset_id !== last_asset_id || row.date !== last_asset_date) {
             last_asset_id = row.asset_id;
             last_asset_date = row.date;
 
