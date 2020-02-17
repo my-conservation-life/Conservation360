cacd52c09261f9829bb3515d404e9d2cef09ccb5
Nikolas Tilley <nmt2400@g.rit.edu>
2020-02-17 15:56:49 -0500

    debugging geom

--- db/api/v1/db/temporal.db.js
+++ db/api/v1/db/temporal.db.js
@@ -64,6 +64,7 @@ const temporalSearch = async (geometry, asset_id, sponsor_name, project_name, as
         break;
     case 'Polygon':
         values.push(utils.db.makeLineStringFromGeoJsonCoordinates(geometry.coordinates));
+        return values;
         query += ' AND ' + POLYGON_WITHIN + ' ';
         break;
     }
