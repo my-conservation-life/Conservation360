4781c35ef7b6be1fac32e3850a4b67c85b63f33e
Nikolas Tilley <nmt2400@g.rit.edu>
2020-02-24 22:10:12 -0500

    debugging

--- db/api/app.js
+++ db/api/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, application/json');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
