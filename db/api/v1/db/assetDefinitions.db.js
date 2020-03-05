71324401d96120358171ef8e95b94b7f2ff58d61
APalumbo97 <ajpalumbo97@gmail.com>
2020-03-05 18:11:23 -0500

    ALSIDLIUASDFUIBSUBDFUIS

--- db/api/v1/db/assetDefinitions.db.js
+++ db/api/v1/db/assetDefinitions.db.js
@@ -72,11 +72,11 @@ const findAssetPropsByTypeID = async (assetTypeID) => {
             ST_Y(asset.location) AS latitude,
             asset_property.value AS value,
             asset_property.property_id AS property_id
-        --     property.is_private AS is_private
+        --    property.is_private AS is_private
         FROM
 	        asset
             JOIN asset_property ON asset_property.asset_id = asset.id
-        --    JOIN property ON asset_property.property_id = property.id
+            JOIN property ON asset_property.property_id = property.id
         WHERE
             asset_type_id = $1
         -- AND
