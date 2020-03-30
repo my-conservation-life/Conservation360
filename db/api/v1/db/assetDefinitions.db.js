0bfe343ac7bc3eda0905d92365b5bf58379b5dde
Anthony Palumbo <ajpalumbo97@gmail.com>
2020-03-30 16:43:09 -0400

    Adding is_private check and locations to Export LWCs. (#89)

--- db/api/v1/db/assetDefinitions.db.js
+++ db/api/v1/db/assetDefinitions.db.js
@@ -46,7 +46,14 @@ const findAssetTypes = async () => {
  */
 const findAssetPropTypes = async (assetTypeID) => {
     let query = PROPERTIES_QUERY;
-    query = query + ' WHERE asset_type_id=$1 ORDER BY id';
+    query = query + `
+        WHERE
+            asset_type_id = $1
+        AND
+            is_private = false
+        ORDER BY
+            id
+    `;
 
     const params = [assetTypeID];
 
@@ -60,16 +67,20 @@ const findAssetPropTypes = async (assetTypeID) => {
 const findAssetPropsByTypeID = async (assetTypeID) => {
     let query = `
         SELECT
-	        asset.id as id, asset_property.value as value, asset_property.property_id as property_id
+            asset.id                    AS id,
+            ST_X(asset.location)        AS longitude,
+            ST_Y(asset.location)        AS latitude,
+            asset_property.value        AS value,
+            asset_property.property_id  AS property_id,
+            property.is_private         AS is_private
         FROM
-	        asset
-
-        INNER JOIN
-	        asset_property
-        ON
-	        asset_property.asset_id=asset.id
+            asset
+            JOIN asset_property     ON asset_property.asset_id = asset.id
+            JOIN property           ON asset_property.property_id = property.id
         WHERE
-	        asset_type_id = $1
+            asset.asset_type_id = $1
+        AND
+            property.is_private = false
         ORDER BY
             property_id
     `;
