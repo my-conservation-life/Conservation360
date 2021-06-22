6504ed33ce54984f8625d6c4bf76f7e4efea14ad
abbynormative <heather@abbynormative.org>
2021-06-22 13:33:51 -0700

    initial commit for planting csv cleaning script

---
+++ db/clean_planting_csv/clean_June_planting_csv.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+data_raw = pd.read_csv('June_planting.csv')
+#print(list(data_raw.columns))
+
+#check if required columns are there
+if set(['asset_id','latitude','longitude']).issubset(data_raw.columns):
+    print('passed check: all required columns in csv')
+
+else: 
+    raise Exception('csv needs asset_id, latitude, and longitude columns in order to process')
+
+#split date/time
+data_raw[['date of planting','time of planting']] = data_raw['date/time of planting'].str.split(' ', 1, expand=True)
+#print(data_raw['date of planting'].head())
+
+#reformat date to dd/mm/yyyy
+data_raw['date of planting'] = pd.to_datetime(data_raw['date of planting']).dt.strftime('%m/%d/%Y')
+#print(data_raw['date of planting'].head())
+
+#delete orig date/time column
+data_raw = data_raw.drop(columns=['date/time of planting'])
+#print(list(data_raw.columns))
+
+#insert underscore for any spaces in column names
+data_raw.columns = data_raw.columns.str.replace(' ', '_')
+#print(list(data_raw.columns))
+
+#any additional reformatting of the time column needed??
+
+#output processed csv
+data_raw.to_csv('June_planting_processed.csv', index=False)
+print('processed csv outputted as June_planting_processed.csv')
