925a162e929632bc7c620b7ba0db1d808f764c13
abbynormative <heather@abbynormative.org>
2021-07-26 10:50:14 -0700

    cleaned up formatting with Black, consolidated some lines, added exceptions

--- db/clean_planting_csv/clean_planting_API_call.py
+++ db/clean_planting_csv/clean_planting_API_call.py
@@ -1,8 +1,8 @@
-import requests, zipfile, io, json, uuid, csv
-import pandas as pd 
+import requests, zipfile, io, json, uuid
+import pandas as pd
 
 if __name__ == "__main__":
-    #set url and email credential variables
+    # set url and email credential variables
     def set_url_email_creds():
         global central_url
         central_url = "https://senecaparkzoo.getodk.cloud"
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         central_email = input("Please enter your email for the ODK API: ")
         central_password = input("Please enter your password for the ODK API: ")
 
-    #gets authentication token
+    # gets authentication token
     def get_email_token():
         email_token_response = requests.post(
             central_url + "/v1/sessions",
@@ -20,11 +20,11 @@ if __name__ == "__main__":
         )
         if email_token_response.status_code == 200:
             return email_token_response.json()["token"]
-        else: 
+        else:
             raise Exception("Invalid authentication response")
 
-    #gets json list of forms and form IDs - may be useful for future data imports
-    #xmlFormId contains the form ID to put into form_response path for retrieving form submissions
+    # gets json list of forms and form IDs - may be useful for future data imports
+    # xmlFormId contains the form ID to put into form_response path for retrieving form submissions
     def get_forms(email_token):
         get_forms_response = requests.get(
             central_url + "/v1/projects/1/forms/",
@@ -32,79 +32,123 @@ if __name__ == "__main__":
         )
         print(get_forms_response.json())
 
-    #retrieves planting form submission data from API and writes to csv
+    # retrieves planting form submission data from API and writes to csv
     def get_planting_form(email_token):
         form_response = requests.get(
-            central_url + "/v1/projects/1/forms/build_Tree-planting_1625058144/submissions.csv",
+            central_url
+            + "/v1/projects/1/forms/build_Tree-planting_1625058144/submissions.csv",
             headers={"Authorization": "Bearer " + email_token},
-            stream=True
+            stream=True,
         )
-        text=form_response.content.decode()
+        text = form_response.content.decode()
         f = open("build_Tree-planting_1625058144.csv", "w")
         f.write(text)
         f.close()
 
-    #logs in to API, calls get_planting_form
+    # logs in to API, calls get_planting_form
     def get_planting_csv():
         set_url_email_creds()
         email_token = get_email_token()
         if email_token:
             get_planting_form(email_token)
-            print("raw planting API data outputted to build_Tree-planting_1625058144.csv")
-        else: 
+            print(
+                "raw planting API data outputted to build_Tree-planting_1625058144.csv"
+            )
+        else:
             raise Exception("Error getting API token")
 
-    #processes and cleans planting data from API
+    # processes and cleans planting data from API
     def clean_planting_csv():
-        #read in data as pandas dataframe for cleaning
+        # read in data as pandas dataframe for cleaning
         data_raw = pd.read_csv("build_Tree-planting_1625058144.csv")
         print("started cleaning and formatting raw data")
 
-        #create asset_id column and fill with shortened uuid to fit in sql bigint field
+        # create asset_id column and fill with shortened uuid to fit in sql bigint field
         data_raw["asset_id"] = ""
         DIGITS = 16
-        data_raw["asset_id"] = [int(uuid.uuid4().hex[:DIGITS], base=16) for _ in range(len(data_raw.index))]
+        data_raw["asset_id"] = [
+            int(uuid.uuid4().hex[:DIGITS], base=16) for _ in range(len(data_raw.index))
+        ]
 
-        #make asset_id the first column
+        # make asset_id the first column
         first_column = data_raw.pop("asset_id")
         data_raw.insert(0, "asset_id", first_column)
 
-        #split date/time column
-        data_raw[["date_of_planting","time_of_planting"]] = data_raw["Time"].str.split("T", 1, expand=True)
+        # split date/time column
+        data_raw[["date_of_planting", "time_of_planting"]] = data_raw["Time"].str.split(
+            "T", 1, expand=True
+        )
 
-        #reformat date to mm/dd/yyyy
-        data_raw["date_of_planting"] = pd.to_datetime(data_raw["date_of_planting"]).dt.strftime("%m/%d/%Y")
+        # reformat date to mm/dd/yyyy
+        data_raw["date_of_planting"] = pd.to_datetime(
+            data_raw["date_of_planting"]
+        ).dt.strftime("%m/%d/%Y")
 
-        #delete unwanted columns
-        data_raw = data_raw.drop(columns=["Time","Location-Accuracy", "Choose_planter","SubmissionDate", "Date", "DeviceID", "KEY"])
-        data_raw = data_raw.drop(columns=["SubmitterID", "AttachmentsPresent", "AttachmentsExpected", "ReviewState", "Edits", "DeviceID.1"])
+        # delete unwanted columns
+        data_raw = data_raw.drop(
+            columns=[
+                "Time",
+                "Location-Accuracy",
+                "Choose_planter",
+                "SubmissionDate",
+                "Date",
+                "DeviceID",
+                "KEY",
+                "SubmitterID",
+                "AttachmentsPresent",
+                "AttachmentsExpected",
+                "ReviewState",
+                "Edits"
+            ]
+        )
+        #deal with second DeviceID column that Pandas can't delete on first pass
+        data_raw = data_raw.drop(
+            columns=[
+                "DeviceID.1"
+            ]
+        )
 
-        #rename meta-instanceID column to clarify difference from uuid in asset_id
+        # rename meta-instanceID column to clarify difference from uuid in asset_id
         data_raw = data_raw.rename(columns={"meta-instanceID": "ODK_meta-instanceID"})
 
-        #complete URL in photo column, rename column to "image_url"
-        #<your-server>/v1/projects/<projectId>/forms/<xmlFormId>/submissions/<instanceId>/attachments/
-        data_raw["Photo_planted_sapling"] = "https://senecaparkzoo.getodk.cloud/v1/projects/1/forms/build_Tree-planting_1625058144/submissions/" + data_raw["ODK_meta-instanceID"].astype(str) + "/attachments/" + data_raw["Photo_planted_sapling"].astype(str)
-        data_raw = data_raw.rename(columns={"Photo_planted_sapling":"image_url"})
+        # complete URL in photo column, rename column to "image_url"
+        # <your-server>/v1/projects/<projectId>/forms/<xmlFormId>/submissions/<instanceId>/attachments/
+        data_raw["Photo_planted_sapling"] = (
+            "https://senecaparkzoo.getodk.cloud/v1/projects/1/forms/build_Tree-planting_1625058144/submissions/"
+            + data_raw["ODK_meta-instanceID"].astype(str)
+            + "/attachments/"
+            + data_raw["Photo_planted_sapling"].astype(str)
+        )
 
-        #clean up other column names
-        data_raw = data_raw.rename(columns={"Location-Latitude":"latitude", "Location-Longitude":"longitude", "QR_Code":"tree_ID"})
-        data_raw = data_raw.rename(columns={"Tree_Species":"species", "SubmitterName":"submitter_name"})
+        data_raw = data_raw.rename(columns={"Photo_planted_sapling": "image_url"})
 
-        #column names to lower case
-        data_raw.columns= data_raw.columns.str.lower()
+        # clean up other column names
+        data_raw = data_raw.rename(
+            columns={
+                "Location-Latitude": "latitude",
+                "Location-Longitude": "longitude",
+                "QR_Code": "tree_ID",
+                "Tree_Species": "species",
+                "SubmitterName": "submitter_name"
+            }
+        )
 
-        #insert underscore for any spaces in column names
+        # column names to lower case
+        data_raw.columns = data_raw.columns.str.lower()
+
+        # insert underscore for any spaces in column names
         data_raw.columns = data_raw.columns.str.replace(" ", "_")
 
-        #double check that required columns are there
-        if set(["asset_id","latitude","longitude"]).issubset(data_raw.columns):
+        # double check that required columns are there
+        if set(["asset_id", "latitude", "longitude"]).issubset(data_raw.columns):
             print("passed check: all required columns in cleaned data")
 
-        else: 
-            raise Exception("csv needs asset_id, latitude, and longitude columns in order to process")
+        else:
+            raise Exception(
+                "csv needs asset_id, latitude, and longitude columns in order to process"
+            )
 
-        #output processed csv
+        # output processed csv
         data_raw.to_csv("planting_processed.csv", index=False)
         print("cleaned planting data csv outputted as planting_processed.csv")
 
